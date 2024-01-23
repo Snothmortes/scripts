131878eb0378e7a2d336b6ffe51ba1cd77820c15
Snothmortes <snothmortes@github.com>
2024-01-23 09:04:12 +0100

    Changes committed on 2024-01-23 09:04:12

--- build/lib/lib/pyhelpers/pytools.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-history_folder = "C:/Users/glenj/AppData/Roaming/Code/User/History/"
-
-
-def clear_history_folder():
-    for path, _, _ in os.walk(history_folder):
-        shutil.rmtree(path)
